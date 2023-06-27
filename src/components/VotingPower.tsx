5dcc0dbd94a879dd3b485bed64ece734debaba07
isp <53378653+isp@users.noreply.github.com>
2023-06-26 22:21:15 -0600

    undo add percentages to storage power

--- src/components/VotingPower.tsx
+++ src/components/VotingPower.tsx
@@ -11,11 +11,6 @@ type Props = {
 };
 
 const VotingPower: FunctionComponent<Props> = ({ yes, no, abstain }) => {
-  const totalVotes = yes + no + abstain;
-  const yesPercentage = (yes / totalVotes) * 100;
-  const noPercentage = (no / totalVotes) * 100;
-  const abstainPercentage = (abstain / totalVotes) * 100;
-
   return (
     <div>
       <div className="text-sm text-neutral-400 mb-3">Voting Power</div>
@@ -23,43 +18,43 @@ const VotingPower: FunctionComponent<Props> = ({ yes, no, abstain }) => {
         <div style={{ width: "100px", height: "100px" }}>
           <CircularProgressbar
             className="basis-1/2 mr-3"
-            value={yesPercentage}
-            text={`${yesPercentage.toFixed(2)}%`}
+            value={yes}
+            text={`${yes}%`}
             styles={buildStyles({
               pathColor: `#228B22`,
               textColor: `#228B22`,
             })}
           />
         </div>
-        <div className="ml-5">{`${yesPercentage.toFixed(2)}%`}</div>
+        <div className="ml-5">{yes} TB</div>
       </div>
       <div className="flex flex-row items-center mb-3">
         <div style={{ width: "100px", height: "100px" }}>
           <CircularProgressbar
             className="basis-1/2 mr-3"
-            value={noPercentage}
-            text={`${noPercentage.toFixed(2)}%`}
+            value={no}
+            text={`${no}%`}
             styles={buildStyles({
               pathColor: `#ED2939`,
               textColor: `#ED2939`,
             })}
           />
         </div>
-        <div className="ml-5">{`${noPercentage.toFixed(2)}%`}</div>
+        <div className="ml-5">{no} TB</div>
       </div>
       <div className="flex flex-row items-center">
         <div style={{ width: "100px", height: "100px" }}>
           <CircularProgressbar
             className="basis-1/2 mr-3"
-            value={abstainPercentage}
-            text={`${abstainPercentage.toFixed(2)}%`}
+            value={abstain}
+            text={`${abstain}%`}
             styles={buildStyles({
               pathColor: `#FDDA0D`,
               textColor: `#FDDA0D`,
             })}
           />
         </div>
-        <div className="ml-5">{`${abstainPercentage.toFixed(2)}%`}</div>
+        <div className="ml-5">{abstain} TB</div>
       </div>
     </div>
   );
